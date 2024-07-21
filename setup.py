678f2500394205155cb18ca00049c38175611b37
JaswanthMannem <mannem.jaswanth1@gmail.com>
2024-07-22 00:23:52 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical Chat Bot",
+    version="0.0.0",
+    author="Mannem Jaswanth",
+    author_email="mannem.jaswanth1@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
